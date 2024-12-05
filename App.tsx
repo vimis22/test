84da330a1ee00b2df4f24324c9e6057a34a497e3
Vivek Misra <115539722+vimis22@users.noreply.github.com>
2024-12-05 17:04:06 +0100

    The Emulator is also working here. The SignupScreen has been fixed in terms of styling.

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './SplashScreen';
 import LoginScreen from './LoginScreen';
+import SignupScreen from './SignupScreen.tsx';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Navigator initialRouteName="SplashScreen">
           <Stack.Screen name="SplashScreen" component={SplashScreen} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="SignupScreen" component={SignupScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
---
+++ SignupScreen.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+
+const SignupScreen = ({ navigation }: any) => {
+  return (
+    <View style={styles.pageContainer}>
+      <Text style={styles.sectionTitle}>SIGNUP</Text>
+      <Text style={styles.textInputLabel}>USERID:</Text>
+      <TextInput style={styles.inputFieldText} placeholder={'Please enter your UserID'}/>
+      <Text style={styles.textInputLabel}>PASSWORD:</Text>
+      <TextInput style={styles.inputFieldText} placeholder={'Please enter your Password'} secureTextEntry={true}/>
+      <Text style={styles.textInputLabel}>CONFIRM PASSWORD:</Text>
+      <TextInput style={styles.inputFieldText} placeholder={'Please confirm your Password'} secureTextEntry={true}/>
+      <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate('RoomListScreen')}>
+        <Text style={styles.buttonText}>REGISTER</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
+        <Text style={styles.buttonText}>Already have an Account? Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+    backgroundColor: '#330099',
+    padding: 20,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  textInputLabel: {
+    color: 'white',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  inputFieldText: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    backgroundColor: '#009930',
+    borderRadius: 5,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: '600',
+    color: 'white',
+  },
+  signupLink: {
+    textAlign: 'center',
+    color: 'white',
+    marginTop: 15,
+    textDecorationLine: 'underline',
+  },
+});
+
+export default SignupScreen;
